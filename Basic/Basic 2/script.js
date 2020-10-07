21f07f62e776ed9d12988420497ce245b5d83145
mackilroy007 <mackilroy007@gmail.com>
2020-10-07 13:02:59 +0200

    initial error fixed

---
+++ Basic/Basic 2/script.js
@@ -0,0 +1,30 @@
+// get data from input form
+$(document).ready(function() {
+    $("button").on("click", function() {
+        var x = $("#tasks").val();
+        console.log(x);
+    });
+});
+    // $("button").on("click").append("#output").text(`
+    // <input type="checkbox" id="task${i+1}" name="task${i+1}" value="${x}">
+    // <label for="task${i+1}">${x}</label><br>
+    // `);
+    // console.log(button);
+
+
+// document.addEventListener("click",getInput,false);
+
+
+
+
+// animation
+$(document).ready(function () {
+    $('li').on('click', function () {
+        $(this).animate({
+            opacity: 0.0,
+            paddingLeft: '+=80'
+        }, 500, function () {
+            $(this).remove();
+        });
+    });
+});
