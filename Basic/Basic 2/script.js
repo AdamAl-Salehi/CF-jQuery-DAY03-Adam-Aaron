44f84a44a1e3026e2e50e14566526eed153003f7
AdamAl-Salehi <email@example.combakr.arkan@icloud.com>
2020-10-07 13:29:26 +0200

    second basic done

--- Basic/Basic 2/script.js
+++ Basic/Basic 2/script.js
@@ -1,10 +1,20 @@
 // get data from input form
 $(document).ready(function() {
     $("button").on("click", function() {
-        var x = $("#tasks").val();
+        x = $("#tasks").val();
         console.log(x);
+        $("ul").append(`<li>${x}</li>`);
+        $('li').on('click', function () {
+            $(this).animate({
+                opacity: 0.0,
+                paddingLeft: '+=80'
+            }, 500, function () {
+                $(this).remove();
+            });
+        });
     });
 });
+
     // $("button").on("click").append("#output").text(`
     // <input type="checkbox" id="task${i+1}" name="task${i+1}" value="${x}">
     // <label for="task${i+1}">${x}</label><br>
@@ -17,14 +27,14 @@ $(document).ready(function() {
 
 
 
-// animation
-$(document).ready(function () {
-    $('li').on('click', function () {
-        $(this).animate({
-            opacity: 0.0,
-            paddingLeft: '+=80'
-        }, 500, function () {
-            $(this).remove();
-        });
-    });
-});
+// // animation
+// $(document).ready(function () {
+//     $('li').on('click', function () {
+//         $(this).animate({
+//             opacity: 0.0,
+//             paddingLeft: '+=80'
+//         }, 500, function () {
+//             $(this).remove();
+//         });
+//     });
+// });
